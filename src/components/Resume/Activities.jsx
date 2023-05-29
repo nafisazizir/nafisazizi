89c0c4aaecea1526d2967dfebbf9f8e61fd607e4
Nafis Azizi Riza <101693218+nafisazizir@users.noreply.github.com>
2023-05-29 12:58:13 +0700

    feat: convert the Activities section to dynamically load from JSON

--- src/components/Resume/Activities.jsx
+++ src/components/Resume/Activities.jsx
@@ -1,61 +1,11 @@
 import React from "react";
 import Collapsible from "react-collapsible";
 import "./ActivitiesStyle.css";
+import cv from "../../content/cv.json";
 
 const Activities = () => {
-  const aiesec = (
-    <>
-      <div className="flex">
-        <h5 className="text-white-primary hover-resume">
-          AIESEC IN UNIVERSITY OF INDONESIA
-        </h5>
-      </div>
-      <div className="flex flex-wrap justify-between">
-        <p className="resume-sub">Project Dev Intern, Business Dev Dept.</p>
-        <p className="resume-year">Oct 2022 - Dec 2022</p>
-      </div>
-    </>
-  );
-
-  const bem = (
-    <>
-      <div className="flex">
-        <h5 className="text-white-primary hover-resume">
-          BEM (STUDENT UNION) FASILKOM UI
-        </h5>
-      </div>
-      <div className="flex flex-wrap justify-between">
-        <p className="resume-sub">Public Relation</p>
-        <p className="resume-year">Mar 2022 - Dec 2022</p>
-      </div>
-    </>
-  );
-
-  const oh = (
-    <>
-      <div className="flex">
-        <h5 className="text-white-primary hover-resume">OPEN HOUSE FASILKOM UI</h5>
-      </div>
-      <div className="flex flex-wrap justify-between">
-        <p className="resume-sub">Vice Project Officer</p>
-        <p className="resume-year">Mar 2022 - Dec 2022</p>
-      </div>
-    </>
-  );
-
-  const compfest = (
-    <>
-      <div className="flex">
-        <h5 className="text-white-primary hover-resume">
-          DATA SCIENCE ACADEMY - COMPFEST 14
-        </h5>
-      </div>
-      <div className="flex flex-wrap justify-between">
-        <p className="resume-sub">Data Science Academy Staff</p>
-        <p className="resume-year">Apr 2022 - Nov 2022</p>
-      </div>
-    </>
-  );
+  const activities = cv.activities;
+  const sizeActivities = activities.length;
 
   return (
     <>
@@ -63,92 +13,43 @@ const Activities = () => {
         <h3 className="text-gradient">Activities</h3>
       </div>
 
-      <Collapsible trigger={aiesec} transitionTime={300} easing="ease-in-out">
-        <ul className="ml-4 text-white-primary">
-          <li>
-            <p>
-              Analyze, and give recommendation/suggestion about the AIESEC in UI
-              program, namely Lead Series
-            </p>
-          </li>
-          <li>
-            <p>
-              Conduct planning to make a new initiative "scholarship program"
-            </p>
-          </li>
-          <li>
-            <p>Create a deck and proposal for the "scholarship program"</p>
-          </li>
-        </ul>
-      </Collapsible>
-
-      <div className="line-horizontal" />
-
-      <Collapsible trigger={bem} transitionTime={300} easing="ease-in-out">
-        <ul className="ml-4 text-white-primary">
-          <li>
-            <p>
-              Conducting one of the Public Relation Bureau namely, Open House
-              Fasilkom UI 2022
-            </p>
-          </li>
-          <li>
-            <p>
-              Being as Humas Partner to the other Bureau and Department in BEM
-              Fasilkom UI
-            </p>
-          </li>
-        </ul>
-      </Collapsible>
-
-      <div className="line-horizontal" />
-
-      <Collapsible trigger={oh} transitionTime={300} easing="ease-in-out">
-        <ul className="ml-4 text-white-primary">
-          <li>
-            <p>
-              Open House Fasilkom UI is an annual event held by BEM Fasilkom UI
-              to promote and intrduce Fasilkom UI to the high school students,
-              and help them to dispel doubts of major that they will take in the
-              university
-            </p>
-          </li>
-          <li>
-            <p>
-              Manage and supervised day to day activities of several divisions:
-              Creative, Web Development, Documentation and Cinematography, and
-              Operational.
-            </p>
-          </li>
-        </ul>
-      </Collapsible>
-
-      <div className="line-horizontal" />
-
-      <Collapsible trigger={compfest} transitionTime={300} easing="ease-in-out">
-        <ul className="ml-4 text-white-primary">
-          <li>
-            <p>
-            Created pre-module for the Data Science Academy, ToR for the speakers
-            </p>
-          </li>
-          <li>
-            <p>
-            Criticizing the curriculum that will be delivered in the academy
-            </p>
-          </li>
-          <li>
-            <p>
-            Liaison Officer to the one of the mentor
-            </p>
-          </li>
-          <li>
-            <p>
-            Conducting administration process registration of the applicants
-            </p>
-          </li>
-        </ul>
-      </Collapsible>
+      {activities.map((activity, i) => {
+        const triggerTitle = (
+          <>
+            <div className="flex">
+              <h5 className="text-white-primary hover-resume">
+                {activity.organization.toUpperCase()}
+              </h5>
+            </div>
+            <div className="flex flex-wrap justify-between">
+              <p className="resume-sub">{activity.title}</p>
+              <p className="resume-year">
+                {activity.startDate} - {activity.endDate}
+              </p>
+            </div>
+          </>
+        );
+        return (
+          <>
+            <Collapsible
+              trigger={triggerTitle}
+              transitionTime={300}
+              easing="ease-in-out"
+            >
+              <ul className="ml-4 text-white-primary">
+                {activity.highlights.map((highlight) => {
+                  return (
+                    <>
+                      <li>{highlight}</li>
+                    </>
+                  );
+                })}
+              </ul>
+            </Collapsible>
+            {i < sizeActivities - 1 ? <div className="line-horizontal" /> : null}
+          </>
+        );
+      })}
     </>
   );
 };
