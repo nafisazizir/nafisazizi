3a92da8ab9ee52975fc8d464beb3dc60fe9e2ad9
Nafis Azizi Riza <101693218+nafisazizir@users.noreply.github.com>
2023-05-29 12:27:49 +0700

    feat: convert the Education section to dynamically load from JSON

--- src/components/Resume/Education.jsx
+++ src/components/Resume/Education.jsx
@@ -1,33 +1,11 @@
 import React from "react";
 import Collapsible from "react-collapsible";
 import "./EducationStyle.css";
+import cv from "../../content/cv.json";
 
 const Education = () => {
-  const ui = (
-    <>
-      <div className="flex">
-        <h5 className="text-white-primary hover-resume">UNIVERSITY OF INDONESIA</h5>
-      </div>
-      <div className="flex flex-wrap justify-between">
-        <p className="resume-sub">Computer Science, Bachelor Degree</p>
-        <p className="resume-year">Aug 2021 - Jun 2025</p>
-      </div>
-    </>
-  );
-
-  const nus = (
-    <>
-      <div className="flex">
-        <h5 className="text-white-primary hover-resume">
-          NATIONAL UNIVERSITY OF SINGAPORE
-        </h5>
-      </div>
-      <div className="flex flex-wrap justify-between">
-        <p className="resume-sub">Computer Science, Exchange Semester</p>
-        <p className="resume-year">Jan 2023 - May 2023</p>
-      </div>
-    </>
-  );
+  const edus = cv.education;
+  const sizeEdus = edus.length;
 
   return (
     <>
@@ -35,36 +13,43 @@ const Education = () => {
         <h3 className="text-gradient">Education</h3>
       </div>
 
-      <Collapsible trigger={ui} transitionTime={300} easing="ease-in-out">
-        <ul className="ml-4 text-white-primary">
-          <li>
-            <p>Bachelor of Computer Science / Sarjana Ilmu Komputer (S.Kom)</p>
-          </li>
-          <li>
-            <p>
-              Relevant Coursework: Data Structure & Algorithm, Platform Based
-              Development, Database, Operating System, Computer Organization
-            </p>
-          </li>
-        </ul>
-      </Collapsible>
-
-      <div className="line-horizontal" />
-
-      <Collapsible trigger={nus} transitionTime={300} easing="ease-in-out">
-        <ul className="ml-4 text-white-primary">
-          <li>
-            <p>Exchange students under ASEAN University Network Program</p>
-          </li>
-          <li>
-            <p>
-              Coursework: Design and Analysis of Algorithm, Interaction Design,
-              Computer Network, Software Engineering & OOP, Programming
-              Methodology II
-            </p>
-          </li>
-        </ul>
-      </Collapsible>
+      {edus.map((edu, i) => {
+        const triggerTitle = (
+          <>
+            <div className="flex">
+              <h5 className="text-white-primary hover-resume">
+                {edu.institution.toUpperCase()}
+              </h5>
+            </div>
+            <div className="flex flex-wrap justify-between">
+              <p className="resume-sub">{edu.degree}</p>
+              <p className="resume-year">
+                {edu.startDate} - {edu.endDate}
+              </p>
+            </div>
+          </>
+        );
+        return (
+          <>
+            <Collapsible
+              trigger={triggerTitle}
+              transitionTime={300}
+              easing="ease-in-out"
+            >
+              <ul className="ml-4 text-white-primary">
+                {edu.highlights.map((highlight) => {
+                  return (
+                    <>
+                      <li>{highlight}</li>
+                    </>
+                  );
+                })}
+              </ul>
+            </Collapsible>
+            {i < sizeEdus - 1 ? <div className="line-horizontal" /> : null}
+          </>
+        );
+      })}
     </>
   );
 };
