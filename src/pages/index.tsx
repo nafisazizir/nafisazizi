c2b5634288c09cc406c1c03cec80d0027727804b
Nafis Azizi Riza <101693218+nafisazizir@users.noreply.github.com>
2024-01-12 22:09:11 +0700

    refactor: remove old hero

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,14 @@
 import React from "react";
 import Layout from "../app-components/Layout/Layout";
-import Hero from "../containers/Hero/Hero";
 import Featured from "../containers/Featured/Featured";
 import MarqueeSlider from "../containers/MarqueeSlider/MarqueeSlider";
 import Works from "../containers/Works/Works";
-import HeroNew from "../containers/HeroNew/HeroNew";
+import Hero from "../containers/Hero/Hero";
 
 const Home = () => {
   return (
     <Layout>
-      <HeroNew />
-      {/* <Hero /> */}
+      <Hero />
       <Featured />
       <MarqueeSlider />
       <Works />
