f4d4e8e0391037b0842e8748ff64ebb6f1fa7de6
Nafis Azizi Riza <101693218+nafisazizir@users.noreply.github.com>
2024-01-20 22:35:11 +0700

    Revert "feat: update footer"
    This reverts commit 03a59c97d2d029ce6668d801f9c0f99b7b506a82.

--- src/app-components/Footer/Footer.tsx
+++ src/app-components/Footer/Footer.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import "./style.css";
+import Logo from "../../assets/icons/logo.svg";
 import LinkedIn from "../../assets/icons/linkedin.svg";
 import Github from "../../assets/icons/github.svg";
-import Button from "../../component/Button/Button";
-import NavigationItem from "../../component/NavigationItem/NavigationItem";
-import Envelope from "../../assets/icons/envelope.svg";
+import Email from "../../assets/icons/email.svg";
+import ButtonCircleSmall from "../Button/Circle/ButtonCircleSmall";
 
 const Footer = () => {
   return (
     <div className="footer-container">
       <div className="footer">
-        <NavigationItem to="/" type="workspace" />
+        <Logo className="nav-logo" />
 
         <div
           style={{
@@ -20,26 +20,42 @@ const Footer = () => {
             alignItems: "center",
           }}
         >
-          <Button
-            type="secondary"
-            size="sm"
-            icon={<LinkedIn />}
+          <ButtonCircleSmall
+            children={
+              <LinkedIn
+                style={{
+                  width: "32px",
+                  height: "32px",
+                  color: "var(--typography-primary)",
+                }}
+              />
+            }
             onClick={() =>
               window.open("https://www.linkedin.com/in/nafisazizi/")
             }
           />
-          <Button
-            type="secondary"
-            size="sm"
-            icon={<Github />}
-            onClick={() =>
-              window.open("https://www.linkedin.com/in/nafisazizi/")
+          <ButtonCircleSmall
+            children={
+              <Github
+                style={{
+                  width: "18px",
+                  height: "18px",
+                  color: "var(--typography-primary)",
+                }}
+              />
             }
+            onClick={() => window.open("https://github.com/nafisazizir")}
           />
-          <Button
-            type="secondary"
-            size="sm"
-            icon={<Envelope />}
+          <ButtonCircleSmall
+            children={
+              <Email
+                style={{
+                  width: "22px",
+                  height: "22px",
+                  color: "var(--typography-primary)",
+                }}
+              />
+            }
             onClick={() =>
               (window.location.href = "mailto:nafisredzone@gmail.com")
             }
