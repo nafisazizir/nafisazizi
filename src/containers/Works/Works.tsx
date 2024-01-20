a062931566944cd9d4952ee1a180632af798ef11
Nafis Azizi Riza <101693218+nafisazizir@users.noreply.github.com>
2024-01-20 22:34:23 +0700

    Revert "feat: add reveal"
    This reverts commit 538bd82daf200167212c09785aa09fdf798d2e90.

--- src/containers/Works/Works.tsx
+++ src/containers/Works/Works.tsx
@@ -3,7 +3,6 @@ import { useStaticQuery, graphql, navigate } from "gatsby";
 import ProjectCard from "../../app-components/ProjectCard/ProjectCard";
 import Button from "../../component/Button/Button";
 import LightBulb from "../../assets/icons/light-bulb.svg";
-import Reveal from "../../component/Reveal/Reveal";
 import "./style.css";
 
 const Works = () => {
@@ -36,35 +35,33 @@ const Works = () => {
   const projects = projectsResponse.allMarkdownRemark.nodes;
 
   return (
-    <Reveal>
-      <div className="flex flex-col w-full justify-center items-center gap-16">
-        <div className="w-11/12 lg:w-[800px] text-3xl md:text-4xl lg:text-5xl font-semibold text-black-900 text-center">
-          Turning ideas to life through innovative and impactful{" "}
-          <span className="text-primary-500">projects</span>
-        </div>
-
-        <div className="works-highlights-container">
-          {projects.map((project: any, index: number) => (
-            <ProjectCard
-              title={project.frontmatter.title}
-              description={project.frontmatter.description}
-              slug={project.frontmatter.slug}
-              image={project.frontmatter.featuredImage}
-              date={project.frontmatter.date}
-              type="projects"
-              key={index}
-            />
-          ))}
-        </div>
-
-        <Button
-          content="All projects"
-          size="lg"
-          icon={<LightBulb />}
-          onClick={() => navigate("/projects/")}
-        />
+    <div className="flex flex-col w-full justify-center items-center gap-16">
+      <div className="w-11/12 lg:w-[800px] text-3xl md:text-4xl lg:text-5xl font-semibold text-black-900 text-center">
+        Turning ideas to life through innovative and impactful{" "}
+        <span className="text-primary-500">projects</span>
       </div>
-    </Reveal>
+
+      <div className="works-highlights-container">
+        {projects.map((project: any, index: number) => (
+          <ProjectCard
+            title={project.frontmatter.title}
+            description={project.frontmatter.description}
+            slug={project.frontmatter.slug}
+            image={project.frontmatter.featuredImage}
+            date={project.frontmatter.date}
+            type="projects"
+            key={index}
+          />
+        ))}
+      </div>
+
+      <Button
+        content="All projects"
+        size="lg"
+        icon={<LightBulb />}
+        onClick={() => navigate("/projects/")}
+      />
+    </div>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Layout from "../app-components/Layout/Layout";
 import Featured from "../containers/Featured/Featured";
+import MarqueeSlider from "../containers/MarqueeSlider/MarqueeSlider";
 import Works from "../containers/Works/Works";
 import Hero from "../containers/Hero/Hero";
 
@@ -9,6 +10,7 @@ const Home = () => {
     <Layout page="landing">
       <Hero />
       <Featured />
+      {/* <MarqueeSlider /> */}
       <Works />
     </Layout>
   );
