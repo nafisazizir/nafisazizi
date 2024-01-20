50d27ad910ef0f7d8ea14c09f84b689cb6e86133
Nafis Azizi Riza <101693218+nafisazizir@users.noreply.github.com>
2024-01-20 22:36:28 +0700

    Revert "feat: update navbar"
    This reverts commit b2bd4f93e927b6d13832317e9279a4594fa84d29.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 const figmaVariables = require("./variableToken.ts").theme;
-console.log(figmaVariables);
 
 module.exports = {
   content: [
     "./src/pages/**/*.{js,jsx,ts,tsx}",
     "./src/components/**/*.{js,jsx,ts,tsx}",
-    "./src/app-components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
